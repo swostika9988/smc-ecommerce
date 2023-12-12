438f346d6094322d7fe6e20669c19009693b55b0
swostika9988 <sostikamahato40@gmail.com>
2023-12-12 08:41:45 +0545

    update

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMC Ecommerce</title>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+<body>
+    <h1 class="myheading" >This is my Homepage</h1>
+    <h1 class="font-bold bg-blue-500 text-white text-xl p-7">This is new heading</h1>
+</body>
+</html>
