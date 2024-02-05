2d13c29a72f46bf1e522ae9dcc63edb435df4031
swostika9988 <sostikamahato40@gmail.com>
2024-02-05 08:39:42 +0545

    product

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::middleware('auth')->group(function(){
     Route::get('/product',[ProductController::class, 'index'])->name('product.index');
     Route::get('/product/create',[ProductController::class, 'create'])->name('product.create');
     Route::post('/product/store',[ProductController::class, 'store'])->name('product.store');
-    Route::get('/product/{id}/edit',[ProductController::class, 'edit'])->name('product.store');
+    Route::get('/product/{id}/edit',[ProductController::class, 'edit'])->name('product.edit');
     Route::put('/product/{id}/update',[ProductController::class, 'update'])->name('product.update');
     Route::get('/product/{id}/delete',[ProductController::class, 'delete'])->name('product.delete');
     
