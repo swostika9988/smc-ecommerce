cdb6191f3402d78444514769a450fe1505fc0323
swostika9988 <sostikamahato40@gmail.com>
2024-08-02 13:21:50 +0545

    relatedproduct

--- resources/views/viewproduct.blade.php
+++ resources/views/viewproduct.blade.php
@@ -29,13 +29,12 @@
     <h2 class="mt-10 text-2xl font-bold px-24">Related Products</h2>
     <diV class="grid grid-cols-4 gap-10 pb-10 px-24">
         @foreach($relatedproducts as $product)
-        <div class="border p-4">
-            <img src="{{asset('images/products/'.$product->photopath)}}" class="w-full h-32 " alt="">
+        <a href="{{route('viewproduct',$product->id)}}" class="border p-4 rounded shadow hover:shadow-md hover:translate-y-1 duration-500">
+            <img src="{{asset('images/products/'.$product->photopath)}}" class="w-full h-60 " alt="">
             <h2 class="text-xl font-bold my-2">{{$product->name}}</h2>
             <p class="text-gray-700 line-clamp-2 ">{{$product->description}}</p>
             <p class="text-gray-700 font-bold mt-2">Rs {{$product->price}}</p>
-            <a href="{{route('viewproduct',$product->id)}}" class="bg-blue-700 text-white py-2 px-4 mt-2 inline-block rounded">View</a>
-        </div>
+        </a>
     @endforeach
     </diV>
     @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,12 @@
 </h1>
 <diV class="grid grid-cols-4 gap-10 pb-10 px-24">
     @foreach($products as $product)
-    <div class="border p-4">
-        <img src="{{asset('images/products/'.$product->photopath)}}" class="w-full h-32 " alt="">
+    <a href="{{route('viewproduct',$product->id)}}" class="border p-4 rounded shadow hover:shadow-md hover:translate-y-1 duration-500">
+        <img src="{{asset('images/products/'.$product->photopath)}}" class="w-full h-60 " alt="">
         <h2 class="text-xl font-bold my-2">{{$product->name}}</h2>
         <p class="text-gray-700 line-clamp-2 ">{{$product->description}}</p>
         <p class="text-gray-700 font-bold mt-2">Rs {{$product->price}}</p>
-        <a href="{{route('viewproduct',$product->id)}}" class="bg-blue-700 text-white py-2 px-4 mt-2 inline-block rounded">View</a>
-    </div>
+    </a>
 @endforeach
 </diV>
 @endsection
